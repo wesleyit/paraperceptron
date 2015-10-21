75cf3978fcc5093b6acd176f925a878088358101
Wesley Rodrigues da Silva <wesley.it@gmail.com>
2015-10-20 22:49:44 -0200

    Improved code indentation.
    Changed the code to use spaces instead tabs.
    Moved the vim comment to the end of each edited file.

--- lib/paraperceptron.ex
+++ lib/paraperceptron.ex
@@ -1,39 +1,38 @@
-# encoding: UTF-8
-# vim: filetype=elixir
-
 defmodule Paraperceptron do
-	@moduledoc """
-	Provide an easy way to deal with Paraconsistent Logic RNAs.
-	Wesley Rodrigues <wesley.it@gmail.com>
-	"""
+  @moduledoc """
+  Provide an easy way to deal with Paraconsistent Logic RNAs.
+  Wesley Rodrigues <wesley.it@gmail.com>
+  """
 
-	@doc """
-	Favorable evidence level(favorable input, unfavorable input) 
-	"""
-	def fel({mi, lambda}), do: mi - lambda
+  @doc """
+  Favorable evidence level(favorable input, unfavorable input) 
+  """
+  def fel({mi, lambda}), do: mi - lambda
 
-	@doc """
-	Unfavorable evidence level(favorable input, unfavorable input) 
-	"""
-	def uel({mi, lambda}), do: (mi + lambda) - 1
+  @doc """
+  Unfavorable evidence level(favorable input, unfavorable input) 
+  """
+  def uel({mi, lambda}), do: (mi + lambda) - 1
 
-	@doc """
-	Get the calculated output({favorable input, unfavorable input}, [limits])
-	The limits are:
-	 - hvsc = higher value of sure control
-	 - lvsc = lower value of sure control
-	 - hvcc = higher value of contradiction control
-	 - lvcc = lower value of contradiction control
-	 Those constants will define the acceptable limits for answers.
-	"""
-	def output({mi, lambda}, limits \\ [hvsc: 0.5, lvsc: -0.5, hvcc: 0.5, lvcc: -0.5]) do
-		cond do 
-			fel({mi, lambda}) >= limits[:hvsc] -> :it_is_true
-			fel({mi, lambda}) <= limits[:lvsc] -> :it_is_false
-			uel({mi, lambda}) >= limits[:hvcc] -> :it_is_inconsistent
-			uel({mi, lambda}) >= limits[:lvcc] -> :it_is_paracomplete
-			true -> :help
-		end
-	end
+  @doc """
+  Get the calculated output({favorable input, unfavorable input}, [limits])
+  The limits are:
+   - hvsc = higher value of sure control
+   - lvsc = lower value of sure control
+   - hvcc = higher value of contradiction control
+   - lvcc = lower value of contradiction control
+   Those constants will define the acceptable limits for answers.
+  """
+  def output({mi, lambda}, limits \\ [hvsc: 0.5, lvsc: -0.5, hvcc: 0.5, lvcc: -0.5]) do
+    cond do 
+      fel({mi, lambda}) >= limits[:hvsc] -> :it_is_true
+      fel({mi, lambda}) <= limits[:lvsc] -> :it_is_false
+      uel({mi, lambda}) >= limits[:hvcc] -> :it_is_inconsistent
+      uel({mi, lambda}) >= limits[:lvcc] -> :it_is_paracomplete
+      true -> :help
+    end
+  end
 
 end
+
+# vim: filetype=elixir tabstop=2 shiftwidth=4 expandtab
--- test/paraperceptron_test.exs
+++ test/paraperceptron_test.exs
@@ -1,6 +1,3 @@
-# encoding: UTF-8
-# vim: filetype=elixir
-
 defmodule ParaperceptronTest do
   use ExUnit.Case
   doctest Paraperceptron
@@ -9,16 +6,18 @@ defmodule ParaperceptronTest do
     assert Paraperceptron.fel({1, 1}) == 0
   end
 
-	test "The method uel is returning properly with {3, 3}." do
+  test "The method uel is returning properly with {3, 3}." do
     assert Paraperceptron.uel({3, 3}) == 5
   end
 
-	test "The method output is returning properly in all cases." do
-		assert Paraperceptron.output({1, 0}) == :it_is_true
-		assert Paraperceptron.output({0, 1}) == :it_is_false
-		assert Paraperceptron.output({1, 1}) == :it_is_inconsistent
-		assert Paraperceptron.output({0.5, 0.5}) == :it_is_paracomplete
-		assert Paraperceptron.output({0, 0}) == :help
-	end
+  test "The method output is returning properly in all cases." do
+    assert Paraperceptron.output({1, 0}) == :it_is_true
+    assert Paraperceptron.output({0, 1}) == :it_is_false
+    assert Paraperceptron.output({1, 1}) == :it_is_inconsistent
+    assert Paraperceptron.output({0.5, 0.5}) == :it_is_paracomplete
+    assert Paraperceptron.output({0, 0}) == :help
+  end
 
 end
+
+# vim: filetype=elixir tabstop=2 shiftwidth=4 expandtab
