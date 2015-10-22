2d2e860001df1c5f1e53696af100fd6e69a4ad03
Wesley Rodrigues da Silva <wesley.it@gmail.com>
2015-10-22 18:10:28 -0200

    Changed the parameters format
    All functions inside the paraperceptron.ex file were configured to
    receive a tuple. As there are not plans to increase the number of
    parameters, they have been changed to simple comma-separated values
    instead tuples.

--- lib/paraperceptron.ex
+++ lib/paraperceptron.ex
@@ -7,12 +7,12 @@ defmodule Paraperceptron do
   @doc """
   Favorable evidence level(favorable input, unfavorable input) 
   """
-  def fel({mi, lambda}), do: mi - lambda
+  def fel(mi, lambda), do: mi - lambda
 
   @doc """
   Unfavorable evidence level(favorable input, unfavorable input) 
   """
-  def uel({mi, lambda}), do: (mi + lambda) - 1
+  def uel(mi, lambda), do: (mi + lambda) - 1
 
   @doc """
   Get the calculated output({favorable input, unfavorable input}, [limits])
@@ -23,12 +23,12 @@ defmodule Paraperceptron do
    - lvcc = lower value of contradiction control
    Those constants will define the acceptable limits for answers.
   """
-  def output({mi, lambda}, limits \\ [hvsc: 0.5, lvsc: -0.5, hvcc: 0.5, lvcc: -0.5]) do
+  def output(mi, lambda, limits \\ [hvsc: 0.5, lvsc: -0.5, hvcc: 0.5, lvcc: -0.5]) do
     cond do 
-      fel({mi, lambda}) >= limits[:hvsc] -> :it_is_true
-      fel({mi, lambda}) <= limits[:lvsc] -> :it_is_false
-      uel({mi, lambda}) >= limits[:hvcc] -> :it_is_inconsistent
-      uel({mi, lambda}) >= limits[:lvcc] -> :it_is_paracomplete
+      fel(mi, lambda) >= limits[:hvsc] -> :it_is_true
+      fel(mi, lambda) <= limits[:lvsc] -> :it_is_false
+      uel(mi, lambda) >= limits[:hvcc] -> :it_is_inconsistent
+      uel(mi, lambda) >= limits[:lvcc] -> :it_is_paracomplete
       true -> :help
     end
   end
