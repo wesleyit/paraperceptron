905562d230900e24bb45d60ab8ad63769d2ab835
Wesley Rodrigues da Silva <wesley.it@gmail.com>
2015-10-25 01:05:30 -0200

    Changed the decision unit to use cond instead if.

--- lib/decision/decision.ex
+++ lib/decision/decision.ex
@@ -14,10 +14,10 @@ defmodule Paraperceptron.Decision do
   @doc """
   Get the calculated output, which is a decision.
   The factors it needs to get a valid response are:
-   - dtf - decision tolerance factor
-   - ctf - contradiction tolerance factor
+  - dtf - decision tolerance factor
+  - ctf - contradiction tolerance factor
   There is a basic equation used to get the calculated favorable level:
-   - rtl - basic structural equation
+  - rtl - basic structural equation
   Usage:
   output(mi, lambda), where mi and lambda are numbers 
   between 0 and 1.
@@ -25,17 +25,16 @@ defmodule Paraperceptron.Decision do
   or the atom :help indicating problems. 
   """
   def output(mi, lambda, factors \\ @default_factors) do
-    if uel(mi, lambda) >= factors[:ctf] do 
-      :help
-    else
-      mir = rtl(mi, lambda)
-      t_limit = f_limit = limit_values(factors[:dtf])
-      return = 0.5
-      if mir > t_limit, do: return = 1
-      if mir < f_limit, do: return = 0
+    mir = rtl(mi, lambda)
+    t_limit = f_limit = limit_values(factors[:dtf])
+    this_uel = uel(mi, lambda)
+    cond do 
+      this_uel >= factors[:ctf] -> :help
+      mir <= f_limit -> 0
+      mir <= t_limit && mir >= f_limit -> 0.5
+      mir >= t_limit -> 1
     end
-    return
   end
 
 end
-# vim: filetype=elixir tabstop=2 shiftwidth=4 expandtab
+# vim: filetype=elixir tabstop=2 shiftwidth=2 expandtab
