9d6ef88f306b1fac0671dd534a831e3429fda646
Wesley Rodrigues da Silva <wesley.it@gmail.com>
2015-10-26 09:03:23 -0200

    Added the learning unit. This is the unic used to learn patterns.

---
+++ lib/learning/learning.ex
@@ -0,0 +1,25 @@
+defmodule Paraperceptron.Learning do
+  import Paraperceptron  
+  @moduledoc """
+  The learning unity of a Paraperceptron Artificial Neural Net.
+  This is an inportant piece of Paraperceptron ANNs, it learns 
+  patterns to get adjusted values.
+  Wesley Rodrigues <wesley.it@gmail.com>
+  """
+  
+  ## The default value for mi2 and lf (learning factor):
+  @default_mi2 0.5
+  @lf 1
+
+  @doc """
+  """
+  def output(mi1, mi2 \\ @default_mi2, desired, step \\ 1) do
+    mi = mi1
+    lambda = 1 - mi2
+    mir = rtl(mi, lambda, @lf)
+    IO.puts("Step: #{step}, Mi: #{mi}, Lambda: #{lambda}, Mir: #{mir}, Desired: #{desired}")
+    if mir != desired, do: output(mi, mir, desired, step + 1)
+  end
+
+end
+# vim: filetype=elixir tabstop=2 shiftwidth=2 expandtab
